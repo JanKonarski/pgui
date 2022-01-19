2a2a62b6d346f69f37f1195ef92419b6ac14d3cc
Michał Wójcik <01153102@pw.edu.pl>
2022-01-19 16:19:28 +0100

    Daily tips

--- src/components/widget/styled_widget/StyledDailyTips.js
+++ src/components/widget/styled_widget/StyledDailyTips.js
@@ -4,7 +4,7 @@ import Widget from "../Widget";
 export const StyledDailyTips = styled(Widget) `
  
   .daily-tips {
-    margin-top: 30px;
+    margin-top: 25px;
     font-size: medium;
     overflow-y: auto;
     height: auto;
